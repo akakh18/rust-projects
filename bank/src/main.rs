a761e2e6cd38ee3beb2c071b79123cb730a029dc
Avto <akakh18@freeuni.edu.ge>
2025-08-25 14:13:44 +0400

    Finish bank miniproject

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -9,6 +9,20 @@ impl Account {
     fn new(id: u32, holder: String) -> Self {
         Account {id, holder, balance: 0}
     }
+
+    fn summary(&self) -> String {
+        format!{"{} has balance of {}", self.holder, self.balance}
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
 }
 
 #[derive(Debug)]
@@ -20,6 +34,18 @@ impl Bank {
     fn new() -> Self {
         Bank {accounts: vec![]}
     }
+
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts.iter().map(|account| account.summary()).collect::<Vec<String>>()
+    }
 }
 
 fn add_account(bank: &mut Bank, account: Account) {
@@ -27,16 +53,22 @@ fn add_account(bank: &mut Bank, account: Account) {
 }
 
 fn print_account(account: Account) {
-    let bank = Bank::new();
     let account = Account::new(1, String::from("me"));
-
     println!("{:#?}", account);
 }
 
 fn main() {
     let mut bank = Bank::new();
-    let account = Account::new(1, String::from("me"));
+    let mut account = Account::new(1, String::from("me"));
+    account.deposit(500);
+    account.withdraw(250);
 
-    add_account(&mut bank, account);
+    println!("{}", account.summary());
+
+    bank.add_account(account);
+
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
+
     println!("{:#?}", bank);
 }
