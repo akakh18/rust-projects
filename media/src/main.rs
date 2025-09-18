08506664b63480abb072a5ccd7709edc9d510bab
Avto <akakh18@freeuni.edu.ge>
2025-09-18 20:14:42 +0400

    Start Media subproject

---
+++ media/src/main.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, director: String },
+    Audiobook { title: String },
+}
+
+impl Media {
+    fn description(&self) -> String {
+        if let Media::Book { title, author } = self {
+            format!("Book: {} {}", title, author)
+        } else if let Media::Movie { title, director } = self {
+            format!("Movie: {} {}", title, director)
+        } else if let Media::Audiobook { title } = self {
+            format!("Audiobook: {}", title)
+        } else {
+            String::from("Media description")
+        }
+    }
+}
+
+fn print_media(media: Media) {
+    println!("{:#?}", media);
+}
+
+fn main() {
+    let audiobook = Media::Audiobook {
+        title: String::from("An Audiobook"),
+    };
+    let good_movie = Media::Movie {
+        title: String::from("Good Movie"),
+        director: String::from("Good Director"),
+    };
+    let bad_book = Media::Book {
+        title: String::from("Bad Book"),
+        author: String::from("Bad Author"),
+    };
+
+    println!("{}", audiobook.description());
+    println!("{}", good_movie.description());
+    println!("{}", bad_book.description());
+}
